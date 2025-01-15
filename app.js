3af3bd2c1bffecabe85d7b7faa69f3fc0ed65151
JosueGaray0877 <159966221+JosueGaray0877@users.noreply.github.com>
2025-01-15 15:12:48 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombreAmigo = input.value.trim();
+
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    actualizarListaAmigos();
+    input.value = "";
+}
+
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    amigos.forEach((amigo, index) => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function obtenerIndiceAleatorio() {
+    return Math.floor(Math.random() * amigos.length);
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    const indiceAleatorio = obtenerIndiceAleatorio();
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `El amigo sorteado es: ${amigoSorteado}`;
+}
+
+
